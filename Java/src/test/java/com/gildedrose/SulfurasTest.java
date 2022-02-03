ef8dc2fcf6281b399c165ec0da2f4f3b8eda3216
David Whitlock <david.m.whitlock@gmail.com>
2022-02-02 19:55:23 -0800

    Use the actual classes in the TextTestFixture.

--- Java/src/test/java/com/gildedrose/SulfurasTest.java
+++ Java/src/test/java/com/gildedrose/SulfurasTest.java
@@ -2,7 +2,19 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class SulfurasTest extends NonConjuredItemTest {
+
+    @Test
+    public void qualityNeverChanges() {
+        int quality = 100;
+        Sulfuras sulfuras = new Sulfuras(10, quality);
+        for (int day = 0; day < 100; day++) {
+            assertThat(sulfuras.quality, equalTo(quality));
+        }
+    }
 
     @Test
     void sameQualityAfter10Days() {
